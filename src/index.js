80d29a2985025135ce57fdd74d798af681cea02a
shaonq <shaonq@qq.com>
2022-06-08 16:25:16 +0800

    v1.1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import utils from "./utils/index"
+import base from './utils/base'
+import date from './utils/date'
+import dialog from './utils/dialog'
+import dom from './utils/dom'
+import util from './utils/util'
+
 import "./index.scss"
-export default utils
+export default {
+    ...base,
+    dom,
+    ...dialog,
+    date,
+    ...util,
+}
